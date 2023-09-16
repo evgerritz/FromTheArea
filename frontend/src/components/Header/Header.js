4e3c38f21ace9ed69d5cd21040297ba9bd0d773d
Evan Gerritz <evgerritz@gmail.com>
2023-09-16 14:33:56 -0400

    vercel test 2

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <Container className={styles.headerContainer}>
         <p className={styles.headerTitle}>
           <Link href="/">
-            FromTheArea(Ethan)
+            FromTheArea(Ethan)(Evan)
           </Link>
         </p>
       </Container>
